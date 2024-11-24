d77d64f55c2b1ef16872d2980e68a6de85264eb5
Fridolin <fridolin.kerner@gmx.de>
2024-11-24 18:20:51 +0100

    fixed character

--- levels/sidescroller.gd
+++ levels/sidescroller.gd
@@ -1,4 +1,4 @@
-extends CharacterBody2D
+extends Node2D
 
 var drone = preload("res://scenes/drone.tscn")
 # Called when the node enters the scene tree for the first time.
---
+++ scenes/robot_char_2.gd
@@ -0,0 +1,23 @@
+extends CharacterBody2D
+
+@export var JUMP_VELOCITY = -500.0
+@onready var anim_player: AnimationPlayer = $Sprite2D/AnimationPlayer
+
+func _ready():
+	anim_player.play("walk")
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+	
+	if is_on_floor() and anim_player.current_animation == "jump":
+		anim_player.play("walk")
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		anim_player.play("jump")
+		velocity.y = JUMP_VELOCITY
+	
+	
+
+	move_and_slide()
