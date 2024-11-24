b7bbfc4faccf0e6e34013913d28efc2a13fcb22d
Fridolin <fridolin.kerner@gmx.de>
2024-11-24 13:53:17 +0100

    animation stuff

--- scripts/level_1_script.gd
+++ scripts/level_1_script.gd
@@ -4,6 +4,8 @@ extends Node2D
 @onready var check: Sprite2D = $CheckButton/Check
 @onready var button_check_00_pressed: Sprite2D = $CheckButton/ButtonCheck00Pressed
 @onready var button_check_00_idle: Sprite2D = $CheckButton/ButtonCheck00Idle
+@onready var check_button: Node = $CheckButton
+@onready var reviewing_data_label: Label = $ReviewingDataLabel
 
 
 
@@ -11,8 +13,7 @@ func _ready() -> void:
 	Messenger.connect("ANIMATION_FINISHED",handle_animations)
 
 func _on_button_button_down() -> void:
-	#robot.animation_player.play("robot_sus")
-	
+
 	button_check_00_idle.visible = false
 	button_check_00_pressed.visible = true
 
@@ -20,10 +21,15 @@ func _on_button_button_down() -> void:
 func handle_animations(anim_name):
 	if anim_name == "robot_sus":
 		Messenger.emit_signal("LEVEL_WIN")
-
-
+	elif anim_name == "check_fill":
+		check_button.queue_free()
+		robot.visible = true
+		reviewing_data_label.visible = true
+		robot.animation_player.play("robot_sus")
+		
 func _on_button_button_up() -> void:
 	button_check_00_pressed.visible = false
 	button_check_00_idle.visible = true
 	check.visible = true	
 	check.animation_player.play("check_fill")
+	
