180c6ec5d1e0ab4e28f0f5567bb04cc9029a9474
lucasreiner <lucas.reiner@live.co.uk>
2024-11-24 17:54:49 +0100

    ich hab programmiert

--- scenes/drone.gd
+++ scenes/drone.gd
@@ -1,10 +1,11 @@
 extends Area2D
 
 @export var drone_speed: int = 10
+@onready var animation_player: AnimationPlayer = $Sprite2D/AnimationPlayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	animation_player.play("fly")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
